b90e9fac179dd3156d411ce369e679b83bf95ded
monivg <monicav2013@gmail.com>
2023-07-17 09:49:04 -0500

    error - unable to build Hibernate SessionFactory using spring boot jpa

--- src/main/java/org/launchcode/scorekeeperapp/models/data/UserRepository.java
+++ src/main/java/org/launchcode/scorekeeperapp/models/data/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
-
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
