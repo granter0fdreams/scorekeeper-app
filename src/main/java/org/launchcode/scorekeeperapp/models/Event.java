3c86ada62f396fde06054517ea27a59847519ced
Josh Hilliard <125423113+jghilliard@users.noreply.github.com>
2023-07-18 17:16:54 -0500

    Update Event.java

--- src/main/java/org/launchcode/scorekeeperapp/models/Event.java
+++ src/main/java/org/launchcode/scorekeeperapp/models/Event.java
@@ -1,9 +1,42 @@
 package org.launchcode.scorekeeperapp.models;
 
-import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-@Entity
 public class Event extends AbstractEntity{
 
+    //admin is the person who creates the event.
+    private User admin;
+
     private String name;
+
+    //An array of players, no need for a hashmap with (name, id) because the user object will provide a unique ID.
+    private ArrayList<User> players;
+
+    public User getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(User admin) {
+        this.admin = admin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<User> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<User> players) {
+        this.players = players;
+    }
+
+    public Event() {
+    }
 }
