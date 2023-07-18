05bd4296dd29cac28c3b16fe6c6dff23058da91c
monivg <monicav2013@gmail.com>
2023-07-18 09:17:23 -0500

    got form to create account to work on localhost - still need to confirm data is saved

--- src/main/java/org/launchcode/scorekeeperapp/controllers/UserController.java
+++ src/main/java/org/launchcode/scorekeeperapp/controllers/UserController.java
@@ -8,23 +8,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("user")
 public class UserController {
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//
-//    @GetMapping("create-account")
-//    public String displayCreateAccountForm (Model model){
-//        model.addAttribute("user",new User());
-//        return "create_account";
-//    }
-//    @PostMapping("create-account")
-//    public String createAccount(@ModelAttribute("user") User user) {
-//        if (userRepository.existsByUsername(user.getUsername()) || userRepository.existsByEmail(user.getEmail())) {
-//            return "login";
-//        }
-//        userRepository.save(user);
-//        return "redirect:/create_tournament";
-//    }
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("host")
+    public String displayCreateAccountForm (Model model){
+        model.addAttribute("user",new User());
+        return "user/host";
+    }
+    @PostMapping("host")
+    public String createAccount(@ModelAttribute("user") User user) {
+        if (userRepository.existsByUsername(user.getUsername()) || userRepository.existsByEmail(user.getEmail())) {
+            return "user/login";
+        }
+        userRepository.save(user);
+        return "redirect:/events/create";
+    }
 }
 
