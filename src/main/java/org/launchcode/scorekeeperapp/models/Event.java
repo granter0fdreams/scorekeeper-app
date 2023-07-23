694fad2dfbd2bad4a9539298fe98891b092154f5
Josh Hilliard <125423113+jghilliard@users.noreply.github.com>
2023-07-23 11:14:27 -0500

    restarting event creation.
    It's working.

--- src/main/java/org/launchcode/scorekeeperapp/models/Event.java
+++ src/main/java/org/launchcode/scorekeeperapp/models/Event.java
@@ -1,9 +1,37 @@
 package org.launchcode.scorekeeperapp.models;
 
 import javax.persistence.Entity;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Positive;
 
 @Entity
 public class Event extends AbstractEntity{
 
     private String name;
+
+    @Positive
+    @Digits(integer = 5, fraction = 0, message = "Please enter an integer larger than 0.")
+    private Integer holes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getHoles() {
+        return holes;
+    }
+
+    public void setHoles(Integer holes) {
+        this.holes = holes;
+    }
+
+    public Event(String name) {
+        this.name = name;
+    }
+    public Event(){}
 }
