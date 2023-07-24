ed2705593f836d20e392e5b378b9d9653485080b
LightningFrisk <bmxcaleb@gmail.com>
2023-07-24 14:15:10 -0500

    Finishing imports to build.gradle for QR code generation, pulled example from https://github.com/rahul26021999/QrCode-JavaSpringBoot to work from.

--- src/main/java/org/launchcode/scorekeeperapp/controllers/BarcodesController.java
+++ src/main/java/org/launchcode/scorekeeperapp/controllers/BarcodesController.java
@@ -1,20 +1,45 @@
 package org.launchcode.scorekeeperapp.controllers;
 
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import com.google.zxing.WriterException;
+import org.launchcode.scorekeeperapp.models.QRCodeGenerator;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/barcodes")
 public class BarcodesController {
-    //@GetMapping(value = "/barbecue/ean13/{barcode}", produces = MediaType.IMAGE_PNG_VALUE)
-    //public ResponseEntity<BufferedImage> barbecueEAN13Barcode(@PathVariable("barcode") String barcode)
-    //        throws Exception {
-    //    return okResponse(BarbecueBarcodeGenerator.generateEAN13BarcodeImage(barcode));
-    //}
+    private static final String QR_CODE_IMAGE_PATH = "./src/main/resources/static/img/QRCode.png";
+
+    @GetMapping("/")
+    public String getQRCode(Model model)
+    {
+        String medium="https://rahul26021999.medium.com/";
+        String github="https://github.com/rahul26021999";
+
+        byte[] image = new byte[0];
+        try {
+
+            // Generate and Return Qr Code in Byte Array
+            image = QRCodeGenerator.getQRCodeImage(medium,250,250);
+
+            // Generate and Save Qr Code Image in static/image folder
+            QRCodeGenerator.generateQRCodeImage(github,250,250,QR_CODE_IMAGE_PATH);
+
+        } catch (WriterException | IOException e) {
+            e.printStackTrace();
+        }
+        // Convert Byte Array into Base64 Encode String
+        String qrcode = Base64.getEncoder().encodeToString(image);
+
+        model.addAttribute("medium",medium);
+        model.addAttribute("github",github);
+        model.addAttribute("qrcode",qrcode);
+
+        return "qrcode";
+        }
 }
