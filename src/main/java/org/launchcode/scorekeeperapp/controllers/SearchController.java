b166b94a226c4e6693504a0ded2314ce43016d96
BlackRose1311 <mr.powers1311@gmail.com>
2023-07-27 14:01:26 -0500

    search method attempt 3

--- src/main/java/org/launchcode/scorekeeperapp/controllers/SearchController.java
+++ src/main/java/org/launchcode/scorekeeperapp/controllers/SearchController.java
@@ -1,9 +1,12 @@
 package org.launchcode.scorekeeperapp.controllers;
 
+import org.launchcode.scorekeeperapp.models.data.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SearchController {
 
-
+    @Autowired
+    private EventRepository eventRepository;
 }
