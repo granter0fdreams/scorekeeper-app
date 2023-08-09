067f41951e196fd7195e5d44a1dfe34d448e549a
BlackRose1311 <mr.powers1311@gmail.com>
2023-08-08 19:38:26 -0500

    Search error check 2

---
+++ src/main/java/org/launchcode/scorekeeperapp/models/TournamentData.java
@@ -0,0 +1,67 @@
+package org.launchcode.scorekeeperapp.models;
+
+import javax.persistence.Entity;
+import java.util.*;
+
+import static org.aspectj.runtime.internal.Conversions.intValue;
+
+public class TournamentData {
+
+    private static final Map<Integer, Event> events = new HashMap<>();
+
+    public static ArrayList<Event> getAll() {
+        return (ArrayList<Event>) events.values();
+    }
+
+    public TournamentData() {
+    }
+
+    public static ArrayList<Event> findByTypeAndValue(String category, String value, Iterable<Event> allTournaments) {
+        ArrayList<Event> results = new ArrayList();
+        if (category.toLowerCase().equals("tournamentName")) {
+            results = findTournamentByName(value, allTournaments);
+            return results;
+        } else if (category.equals("tournamentId")) {
+            results = findTournamentById(Integer.parseInt(value), allTournaments);
+            return results;
+        }
+        return results;
+    }
+
+    public static ArrayList<Event> findTournamentByName(String value, Iterable<Event> allTournaments) {
+
+        String lower_val = value.toLowerCase();
+
+        ArrayList<Event> results = new ArrayList<>();
+
+        for (Event tournamentName : allTournaments) {
+
+            if (tournamentName.getName().toLowerCase().contains(lower_val)) {
+                results.add(tournamentName);
+            } else {
+                System.out.println("No results found");
+            }
+
+        }
+
+        return results;
+    }
+
+    public static ArrayList<Event> findTournamentById(int tournamentId, Iterable<Event> allTournaments) {
+
+        int eventId = Event.getId();
+
+        ArrayList<Event> results = new ArrayList<>();
+
+        for (Event event : allTournaments) {
+
+            if (intValue(eventId) == intValue(tournamentId)) {
+                results.add(event);
+            }
+
+            return results;
+
+        }
+        return results;
+    }
+}
